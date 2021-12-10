afaee32ad48be0834cd674b49491f3d3f21dacfb
kimdh-hi <zbeld123@gmail.com>
2021-12-10 13:44:54 +0900

    cors설정

--- src/main/java/com/sparta/showmethecode/ShowMeTheCodeApplication.java
+++ src/main/java/com/sparta/showmethecode/ShowMeTheCodeApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,7 +21,15 @@ public class ShowMeTheCodeApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://www.smtc.shop");
+                registry.addMapping("/**").allowedOrigins("http://www.smtc.shop")
+                        .allowedMethods(
+                                HttpMethod.GET.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.HEAD.name(),
+                                HttpMethod.DELETE.name()
+                        )
+                ;
             }
         };
     }
